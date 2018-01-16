9b3bec76b132ce706ad17f65d33163fa8f50acae
Alexey Shapovalov <alexey.notjunk@gmail.com>
2018-01-16 19:19:15 +0200

    minor formatting to toEclipse.bat

--- toEclipse.bat
+++ toEclipse.bat
@@ -1,14 +1,15 @@
 @echo off
 
+echo This file copies all of the intelliJ workspace into eclipse,
+echo it will overwrite anything as needed.
+echo press 'Y' to preform copy, 'N' to exit the program.
+echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 :begin
-echo Are you sure? this will override the current files in eclipse.
+echo Are you sure? this will overwrite the current files in eclipse.
 choice
 if "%ERRORLEVEL%" == "2" goto exit
-robocopy ".\intelliJ\src" ".\Eclipse_final\MotionTesting\src"
 for /d %%i in (".\intelliJ\src\*") do (
-	set "firstVar=%%i"
-	echo %%i
-	echo "%firstVar%"
 	robocopy %%i ".\Eclipse_final\MotionTesting\src\%%~nxi" /e /is
 )
 goto begin
