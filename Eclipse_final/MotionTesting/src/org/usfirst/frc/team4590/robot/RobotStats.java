cd92096833566660f14e3c301185941df16391f3
orr kral <karlor041@gmail.com>
2018-01-18 13:10:14 +0200

    I have no idea

---
+++ Eclipse_final/MotionTesting/src/org/usfirst/frc/team4590/robot/RobotStats.java
@@ -0,0 +1,9 @@
+package org.usfirst.frc.team4590.robot;
+
+public final class RobotStats {
+	
+	public static final double RightLeftWheelDistance = 0.68,
+							   FrontBackWheelDistance = 0.0,
+							   WheelDiameter = 0.0;
+	
+}
