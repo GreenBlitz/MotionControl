ae50f54d10562463ebe92413d1809a1711286faa
LPNG1 <itamar.oryan@gmail.com>
2018-11-28 18:41:27 +0200

    fixed PointTest

--- src/test/java/PointTest.java
+++ src/test/java/PointTest.java
@@ -1,8 +1,7 @@
 import org.greenblitz.motion.Point;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 
 public class PointTest {
@@ -22,9 +21,9 @@ public class PointTest {
         Point p1 = new Point(0, 0);
         Point p2 = new Point(13, -4);
         p1.translate(p2);
-        assertTrue(p1.equals(p2), "translate test");
+        assertEquals(p1, p2, "translate test");
         p2.translate(-10, 0);
-        assertTrue(p1.getY() == p2.getY());
+        assertEquals(p1.getY(), p2.getY(), 0.0);
         assertEquals(p1.getX() - 10, p2.getX());
         p2.translate(p2);
         assertEquals(6, p2.getX());
@@ -34,8 +33,8 @@ public class PointTest {
     @Test
     void cloneTest() {
         Point p1 = new Point(Math.random(), Math.random());
-        assertTrue(p1 != p1.clone());
-        assertTrue(p1.equals(p1.clone()));
+        assertNotSame(p1, p1.clone());
+        assertEquals(p1, p1.clone());
     }
 
     @Test
