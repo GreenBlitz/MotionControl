2d2fd750cc40f5569e33c78f545d57d539b5da86
SoftwareGoodPC <greenblitz.programmers@gmail.com>
2019-01-10 16:21:49 +0200

    continiued Profiler1dTest

--- src/test/java/Profiler1dTest.java
+++ src/test/java/Profiler1dTest.java
@@ -9,6 +9,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class Profiler1dTest {
 
+    static final double EPSILON = 1E-6;
+
     @Test
     void test1 (){
         ArrayList<ActuatorLocation> points = new ArrayList<>();
@@ -16,17 +18,19 @@ public class Profiler1dTest {
         points.add(new ActuatorLocation(10,0));
         try{
             MotionProfile p = Profiler1D.generateProfile(points, 1, 1, -1);
-            assertEquals(p.getAcceleration(0.5), 1);
-            assertEquals(p.getAcceleration(5), 0);
-            assertEquals(p.getAcceleration(10.5), -1);
 
-            assertEquals(p.getVelocity(0.5), 0.5);
-            assertEquals(p.getVelocity(5), 1);
-            assertEquals(p.getVelocity(10.5), 0.5);
+            assertEquals(p.getAcceleration(0.5), 1, EPSILON);
+            assertEquals(p.getAcceleration(5), 0, EPSILON);
+            assertEquals(p.getAcceleration(10.5), -1, EPSILON);
 
-            assertEquals(p.getLocation(0.5), 0.125);
-            assertEquals(p.getLocation(5), 4.5);
-            assertEquals(p.getLocation(10.5), 9.875);
+            assertEquals(p.getVelocity(0.5), 0.5, EPSILON);
+            assertEquals(p.getVelocity(5), 1, EPSILON);
+            assertEquals(p.getVelocity(10.5), 0.5, EPSILON);
+
+            assertEquals(p.getLocation(0.5), 0.125, EPSILON);
+            assertEquals(p.getLocation(5), 4.5, EPSILON);
+            assertEquals(p.getLocation(10.5), 9.875, EPSILON);
+
         }catch(ProfilingException e){
             System.out.println(e);
             assertEquals("good code", "shit code");
@@ -40,17 +44,46 @@ public class Profiler1dTest {
         points.add(new ActuatorLocation(10,0));
         try{
             MotionProfile p = Profiler1D.generateProfile(points, 2, 4, -0.5);
-            assertEquals(p.getAcceleration(0.25), 4);
-            assertEquals(p.getAcceleration(1), 0);
-            assertEquals(p.getAcceleration(10.5), -1);
 
-            assertEquals(p.getVelocity(0.5), 0.5);
-            assertEquals(p.getVelocity(5), 1);
-            assertEquals(p.getVelocity(10.5), 0.5);
+            assertEquals(p.getAcceleration(0.25), 4, EPSILON);
+            assertEquals(p.getAcceleration(1), 0, EPSILON);
+            assertEquals(p.getAcceleration(4), -0.5, EPSILON);
 
-            assertEquals(p.getLocation(0.5), 0.125);
-            assertEquals(p.getLocation(5), 4.5);
-            assertEquals(p.getLocation(10.5), 9.875);
+            assertEquals(p.getVelocity(0.25), 1, EPSILON);
+            assertEquals(p.getVelocity(1), 2, EPSILON);
+            assertEquals(p.getVelocity(4), 1.625, EPSILON);
+
+            assertEquals(p.getLocation(0.25), 0.125, EPSILON);
+            assertEquals(p.getLocation(1), 1.5, EPSILON);
+            assertEquals(p.getLocation(4), 6+3.625/2*0.75, EPSILON);
+
+        }catch(ProfilingException e){
+            System.out.println(e);
+            assertEquals("good code", "shit code");
+        }
+    }
+
+    @Test
+    void test3 (){
+        ArrayList<ActuatorLocation> points = new ArrayList<>();
+        points.add(new ActuatorLocation(0,0));
+        points.add(new ActuatorLocation(15, -1));
+        points.add(new ActuatorLocation(10,0));
+         try{
+            MotionProfile p = Profiler1D.generateProfile(points, 2, 4, -0.5);
+
+            assertEquals(p.getAcceleration(0.25), 4, EPSILON);
+            assertEquals(p.getAcceleration(1), 0, EPSILON);
+            assertEquals(p.getAcceleration(4), -0.5, EPSILON);
+
+            assertEquals(p.getVelocity(0.25), 1, EPSILON);
+            assertEquals(p.getVelocity(1), 2, EPSILON);
+            assertEquals(p.getVelocity(4), 1.625, EPSILON);
+
+            assertEquals(p.getLocation(0.25), 0.125, EPSILON);
+            assertEquals(p.getLocation(1), 1.5, EPSILON);
+            assertEquals(p.getLocation(4), 6+3.625/2*0.75, EPSILON);
+
         }catch(ProfilingException e){
             System.out.println(e);
             assertEquals("good code", "shit code");
