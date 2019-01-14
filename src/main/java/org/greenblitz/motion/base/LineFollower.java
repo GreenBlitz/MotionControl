f9a3d3e91e82755991d5a1d40825c808c9329365
Peleg <peleg.caduri@gmail.com>
2019-01-14 06:01:11 -0800

    tiny error was fixed

--- src/main/java/org/greenblitz/motion/base/LineFollower.java
+++ src/main/java/org/greenblitz/motion/base/LineFollower.java
@@ -1,9 +1,8 @@
 package org.greenblitz.motion.base;
+import  org.greenblitz.motion.pid.PIDController;
 
 public class LineFollower extends PIDController {
     public LineFollower (double m_kp, double m_ki, double m_kd) {
         super(m_kp, m_ki, m_kd);
     }
-
-
 }
