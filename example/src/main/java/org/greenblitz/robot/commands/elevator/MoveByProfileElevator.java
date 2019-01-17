12b986f72036674e4408328065075d2f57dad7be
Savioor <alexey.notjunk@gmail.com>
2019-01-17 18:55:40 +0200

    MoveByProfile is as a Thread now lmao

--- example/src/main/java/org/greenblitz/robot/commands/elevator/MoveByProfileElevator.java
+++ example/src/main/java/org/greenblitz/robot/commands/elevator/MoveByProfileElevator.java
@@ -1,25 +1,25 @@
 package org.greenblitz.robot.commands.elevator;
 
-import org.greenblitz.motion.pid.MultivariablePIDController;
+import edu.wpi.first.wpilibj.command.Command;
 import org.greenblitz.motion.pid.PIDController;
 import org.greenblitz.motion.pid.PIDObject;
 import org.greenblitz.motion.profiling.ActuatorLocation;
 import org.greenblitz.motion.profiling.MotionProfile;
 import org.greenblitz.motion.profiling.Profiler1D;
 import org.greenblitz.motion.profiling.exception.ProfilingException;
-import org.greenblitz.robot.commands.PeriodicCommand;
 import org.greenblitz.robot.subsystems.ElevatorPrototype;
 import org.greenblitz.utils.CSVWrapper;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.List;
 
-public class MoveByProfileElevator extends PeriodicCommand {
+public class MoveByProfileElevator extends Command implements Runnable {
 
     private long startTime;
-    private MotionProfile profile; // MaxV = 3800, MaxA=30000
+    private MotionProfile profile;
     private boolean done = false;
-    private static final double MAX_VEL = 1.5;//63000;
-    private static final double MAX_ACCEL = 115 / 10.0;//79;//33000000;
+    private static final double MAX_VEL = 1.5;
+    private static final double MAX_ACCEL = 115 / 10.0;
     private static final long PERIOD = 1;
 
     private static final double kV = 1.0 / MAX_VEL,
@@ -31,7 +31,7 @@ public class MoveByProfileElevator extends PeriodicCommand {
 
     private CSVWrapper printer;
 
-    public MoveByProfileElevator(ActuatorLocation... locs){
+    public MoveByProfileElevator(ActuatorLocation... locs) {
         this(Arrays.asList(locs));
     }
 
@@ -52,13 +52,12 @@ public class MoveByProfileElevator extends PeriodicCommand {
         locationController = new PIDController(new PIDObject(kP));
 
         System.out.println(profile);
-    }
+    }++++++++
 
     @Override
     protected void initialize() {
-        startTime = System.currentTimeMillis();
+        ElevatorPrototype.getInstance().resetEncoder();
         done = false;
-        start();
     }
 
     @Override
@@ -85,26 +84,30 @@ public class MoveByProfileElevator extends PeriodicCommand {
     }
 
     @Override
-    protected void periodic() {
-        double t = (System.currentTimeMillis() - startTime) / 1000.0;
-
-        if (profile.isOver(t)) {
-            done = true;
-            return;
-        }
-
-        if (printer != null)
-            printer.addValues(t, profile.getVelocity(t), ElevatorPrototype.getInstance().getSpeed());
-
+    public void run() {
+        locationController.init(0, 0);
+        startTime = System.currentTimeMillis();
         ElevatorPrototype prototype = ElevatorPrototype.getInstance();
+        double vPower, aPower, power;
+        while (true) {
+            double t = (System.currentTimeMillis() - startTime) / 1000.0;
 
-        double vPower = kV * profile.getVelocity(t);
-        double aPower = kA * profile.getAcceleration(t);
+            if (profile.isOver(t)) {
+                done = true;
+                return;
+            }
 
-        double power = vPower + aPower + ff + locationController.calculatePID(profile.getLocation(t),
-                prototype.getDistance());
+            if (printer != null)
+                printer.addValues(t, profile.getVelocity(t), ElevatorPrototype.getInstance().getSpeed());
 
-        prototype.set(-power);
+            vPower = kV * profile.getVelocity(t);
+            aPower = kA * profile.getAcceleration(t);
+
+            power = vPower + aPower + ff + locationController.calculatePID(profile.getLocation(t),
+                    prototype.getDistance());
+
+            prototype.set(-power);
+        }
     }
 
 }
