1cbd054afc9f0148c8c457cb3bb79e5252761ea0
Savioor <alexey.notjunk@gmail.com>
2019-01-19 19:05:50 +0200

    Elevator update and some simulation fun

--- example/src/main/java/org/greenblitz/robot/commands/elevator/StopElevator.java
+++ example/src/main/java/org/greenblitz/robot/commands/elevator/StopElevator.java
@@ -5,13 +5,23 @@ import org.greenblitz.robot.subsystems.ElevatorPrototype;
 
 public class StopElevator extends Command {
 
+    private long inceptionTime;
+
     public StopElevator() {
         requires(ElevatorPrototype.getInstance());
     }
 
     @Override
+    protected void initialize() { inceptionTime = System.currentTimeMillis(); }
+
+    @Override
     protected void execute() {
-        ElevatorPrototype.getInstance().set(-0.4);
+        if (System.currentTimeMillis() - inceptionTime < 5000)
+            ElevatorPrototype.getInstance().set(-0.4);
+        else if (ElevatorPrototype.getInstance().getDistance() > 0.1)
+            ElevatorPrototype.getInstance().set(-0.1);
+        else
+            ElevatorPrototype.getInstance().set(0);
     }
 
     @Override
