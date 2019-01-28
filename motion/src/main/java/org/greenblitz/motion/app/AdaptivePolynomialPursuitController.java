965d083791e1a1bb9f69cdca0d0814bc0ec42538
Savioor <alexey.notjunk@gmail.com>
2019-01-29 01:35:05 +0200

    another fix of APPC3

--- motion/src/main/java/org/greenblitz/motion/app/AdaptivePolynomialPursuitController.java
+++ motion/src/main/java/org/greenblitz/motion/app/AdaptivePolynomialPursuitController.java
@@ -45,22 +45,24 @@ public class AdaptivePolynomialPursuitController extends AbstractPositionPursuit
 
         double v2 = Math.tan(ang);
 
-        if (Math.abs(v2) <= 100) { // Angle is less then 0.99*(PI/2) or more then 1.007*(PI/2)
+        if (Math.abs(v2) <= 1000) { // Angle is less then 0.999*(PI/2) or more then 1.001*(PI/2)
 
             double x2 = deltaVect.getX();
+            x2 = Math.max(Math.abs(x2), 1E-6)*Math.signum(x2);
             double y2 = deltaVect.getY();
 
-            return x2 == 0 ? 0 : 2 * (v2 * x2 - 3 * y2) / -Math.pow(x2, 2);
+            return 2 * (v2 * x2 - 3 * y2) / -Math.pow(x2, 2);
         }
 
         deltaVect.rotate(Math.PI / 4);
         double x2 = deltaVect.getX();
+        x2 = Math.max(Math.abs(x2), 10E-6)*Math.signum(x2);
         double y2 = deltaVect.getY();
         v2 = Math.tan(ang + (Math.PI / 4));
         //v1 = Math.tan(Math.PI / 4) = 1;
 
         // 2*b/(1 + 1)**1.5 = 0.707*b
-        return x2 == 0 ? 0 : 0.707*(2 * x2 + v2 * x2 - 3 * y2) / -Math.pow(x2, 2);
+        return x2 == 0 ? 0 : 0.7071*(2 * x2 + v2 * x2 - 3 * y2) / -Math.pow(x2, 2);
     }
 
 }
