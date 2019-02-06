021cd1451636f90e50962355c6fe17476728ced7
GB Software laptop <greenblitz.programmers@gmail.com>
2019-02-06 12:52:17 +0200

    untested - An ability to handle following line with angle in the begining

--- example/src/main/java/org/greenblitz/robot/commands/LineFollowerCommandFLLSensor.java
+++ example/src/main/java/org/greenblitz/robot/commands/LineFollowerCommandFLLSensor.java
@@ -13,7 +13,11 @@ public class LineFollowerCommandFLLSensor extends PeriodicCommand {
     protected LineFollower m_controller;
     protected double distance;
     protected double initDistance;
+    private int countMaxErrorRuns = 0;
     private int side; // right side = 1
+    private double kp;
+    private double kd;
+    private int kdCounter = 0;
 
     // right side = true
     public LineFollowerCommandFLLSensor(long period, double kp, double ki, double kd, double speed, double distance, boolean side) {
@@ -25,6 +29,8 @@ public class LineFollowerCommandFLLSensor extends PeriodicCommand {
         this.distance = distance;
         m_firstRun = true;
         this.side = side ? 1 : -1;
+        this.kp = kp;
+        this.kd = kd;
     }
 
     public LineFollowerCommandFLLSensor(double kp, double ki, double kd, double speed, double distance, boolean side) {
@@ -33,15 +39,26 @@ public class LineFollowerCommandFLLSensor extends PeriodicCommand {
 
     @Override
     protected void periodic() {
+        double value = Chassis.getInstance().getColorSensorValue();
         if (m_firstRun) {
-            m_controller.init(0.45, Chassis.getInstance().getColorSensorValue());
+            m_controller.init(0.45, value);
             initDistance = Chassis.getInstance().getDistance();
             m_firstRun = false;
         }
+        countMaxErrorRuns = (value < 0.17 || value > 0.73)? countMaxErrorRuns + 1: 0;
+        if (countMaxErrorRuns >  5) {
+            m_controller.getPidObject().setKp(1.5*kp);
+            kdCounter = 5;
+        }
+        else if (kdCounter > 0) {
+            m_controller.getPidObject().setKd(2*kd);
+            kdCounter --;
+        }
         m_chassis.arcadeDrive(-speed, -side * m_controller.calculatePID(0.45, Chassis.getInstance().getColorSensorValue()));
-
+        m_controller.getPidObject().setKp(kp);
+        m_controller.getPidObject().setKd(kd);
         synchronized (this) {
-            m_finished = Chassis.getInstance().getDistance() > initDistance + distance;
+            m_finished = Math.abs(Chassis.getInstance().getDistance() - initDistance) > distance;
         }
     }
 
