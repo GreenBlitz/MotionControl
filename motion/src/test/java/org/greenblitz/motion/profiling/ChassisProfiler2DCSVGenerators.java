d68494fe000dad4a9496e9d93420d53dc9edfb3e
Alexey <theemailreader300@gmail.com>
2019-11-11 18:43:03 +0200

    literarly nothing

--- motion/src/test/java/org/greenblitz/motion/profiling/ChassisProfiler2DCSVGenerators.java
+++ motion/src/test/java/org/greenblitz/motion/profiling/ChassisProfiler2DCSVGenerators.java
@@ -23,13 +23,13 @@ public class ChassisProfiler2DCSVGenerators {
     void generate2DProfile() {
         List<State> states = new ArrayList<>();
         states.add(new org.greenblitz.motion.base.State(0, 0, 0, 0, 0));
-        states.add(new org.greenblitz.motion.base.State(0.8, 3, Math.PI/4, 0, 0));
+        states.add(new org.greenblitz.motion.base.State(1, 3, Math.PI/4, 0, 0));
         states.add(new org.greenblitz.motion.base.State(2, 3, Math.PI/2, 0, 0));
 
         long time = System.currentTimeMillis();
         MotionProfile2D brofile = ChassisProfiler2D.generateProfile(
-                pathToState(Paths.get("LTurn", true))
-                , .01, 0.7,
+                states
+                , .005, 0.7,
                 2.1, 4.6, 10, 0, 0.1);
         System.out.println("Full Generation");
         System.out.println(System.currentTimeMillis() - time);
@@ -62,6 +62,7 @@ public class ChassisProfiler2DCSVGenerators {
             locFile.addValues(loc.getX(), loc.getY());
 //            System.out.println(loc);
         }
+        locFile.flush();
 //        ICurve curve = CubicSplineGenerator.generateSpline(states.get(0), states.get(1), 0.8);
 //        for (double u = 0; u <= 1; u += 0.001) {
 //            Point p = curve.getLocation(u);
