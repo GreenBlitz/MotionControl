55f221b387e863313a977a78dec1e4a18bb9ec76
Peleg <peleg.caduri@gmail.com>
2019-11-12 19:25:34 +0200

    progress on QuinticSplineGenerator, finished!

--- motion/src/main/java/org/greenblitz/motion/profiling/curve/spline/QuinticSplineGenerator.java
+++ motion/src/main/java/org/greenblitz/motion/profiling/curve/spline/QuinticSplineGenerator.java
@@ -41,23 +41,21 @@ public class QuinticSplineGenerator {
 
     /**
      * See
-     * https://matrixcalc.org/en/slu.html#solve-using-Gaussian-elimination%28%7B%7B0,0,0,1,0%7D,%7B1,1,1,1,x%7D,%7B0,0,1,0,d_0%7D,%7B3,2,1,0,d_1%7D%7D%29
-     * or this
-     * https://matrixcalc.org/en/slu.html#solve-using-Gaussian-elimination%28%7B%7B0,0,0,1,x_0%7D,%7B1,1,1,1,x_1%7D,%7B0,0,1,0,d_0%7D,%7B3,2,1,0,d_1%7D%7D%29
-     * or this
-     * https://matrixcalc.org/en/slu.html#solve-using-Gaussian-elimination%28%7B%7B0,0,0,1,x_0%7D,%7Bt%5E3,t%5E2,t,1,x_1%7D,%7B0,0,1,0,d_0%7D,%7B3%2At%5E2,2%2At,1,0,d_1%7D%7D%29
      *
-     * (the lower the equation, the more recent)
+     * https://matrixcalc.org/en/slu.html#solve-using-Gaussian-elimination%28%7B%7B0,0,0,0,0,1,x_0%7D,%7Bt%5E5,t%5E4,t%5E3,t%5E2,t,1,x_1%7D,%7B0,0,0,0,1,0,d_0%7D,%7B5%2At%5E4,4%2At%5E3,3%2At%5E2,2%2At,1,0,d_1%7D,%7B0,0,0,2,0,0,D_0%7D,%7B20%2At%5E3,12%2At%5E2,6%2At,2,0,0,D_1%7D%7D%29
      *
-     * @param dervStart
-     * @param dervEnd
+     * @param dervS
+     * @param dervE
      * @return
      */
-    protected static double[] getParams(double dStart, double dEnd, double dervStart, double dervEnd, double derv2Start,double derv2End, double t){
+    protected static double[] getParams(double dStart, double dEnd, double dervS, double dervE, double derv2S,double derv2E, double t){
         double dx = dEnd - dStart;
         double tt = t*t;
         return new double[] {
-                dStart, dervStart, (-2*dervStart*t - dervEnd*t + 3*dx)/tt, (dervStart*t + dervEnd*t - 2*dx)/(t*tt)
+                dStart, dervS, derv2S/2
+                ,(tt*(-3*derv2S + derv2E) - t*(12*dervS + 8*dervE) + 20*dx)/(2*tt*t)
+                ,(tt*(3*derv2S - 2*derv2E) + t*(16*dervS + 14*dervE) - 30*dx)/(2*tt*tt)
+                ,(tt*(-derv2S + derv2E) - 6*t*(dervS + dervE) + 12*dx)/(2*tt*tt*t)
         };
     }
 }
