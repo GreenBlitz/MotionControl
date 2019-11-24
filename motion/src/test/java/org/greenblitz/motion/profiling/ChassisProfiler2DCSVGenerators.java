dcb0cf0bd36708375887750b6b1aecf7c287c202
Alexey <theemailreader300@gmail.com>
2019-11-24 23:27:34 +0200

    some comments mostly

--- motion/src/test/java/org/greenblitz/motion/profiling/ChassisProfiler2DCSVGenerators.java
+++ motion/src/test/java/org/greenblitz/motion/profiling/ChassisProfiler2DCSVGenerators.java
@@ -24,14 +24,17 @@ public class ChassisProfiler2DCSVGenerators {
     void generate2DProfile() {
         List<State> states = new ArrayList<>();
         states.add(new org.greenblitz.motion.base.State(0, 0, 0, 0, 0));
-        states.add(new org.greenblitz.motion.base.State(1, 3, Math.PI/4, 0.01, 0.5));
-        states.add(new org.greenblitz.motion.base.State(2, 3, Math.PI/2, 0, 0));
+        states.add(new org.greenblitz.motion.base.State(1, 1, Math.PI/4, 1,2));
+        states.add(new org.greenblitz.motion.base.State(2, 2, Math.PI/2, 1,0.5));
+        states.add(new org.greenblitz.motion.base.State(3, 3, Math.PI/4, 1,4));
+        states.add(new org.greenblitz.motion.base.State(4, 4, Math.PI/2, 1, 0.25));
+        states.add(new org.greenblitz.motion.base.State(5, 5, Math.PI/4, 0,0));
 
         long time = System.currentTimeMillis();
         MotionProfile2D brofile = ChassisProfiler2D.generateProfile(
                 states//pathToState(Paths.get("LTurn", true))
                 , .001, 0.7,
-                2.1, 4.6, 10, 0, 1f);
+                2.1, 4.6, 10, 0, 2f);
         System.out.println("Full Generation");
         System.out.println(System.currentTimeMillis() - time);
 
