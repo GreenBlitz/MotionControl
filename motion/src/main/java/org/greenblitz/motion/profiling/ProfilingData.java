52a9891c8284cadde3800d9a692ee52a4cc50e76
Alexey <theemailreader300@gmail.com>
2019-12-04 17:18:22 +0200

    cleanup

---
+++ motion/src/main/java/org/greenblitz/motion/profiling/ProfilingData.java
@@ -0,0 +1,57 @@
+package org.greenblitz.motion.profiling;
+
+import java.util.Objects;
+
+public class ProfilingData {
+
+    protected double maxLinearVelocity, maxLinearAccel, maxAngularVelocity, maxAngularAccel;
+
+    public ProfilingData(double maxLinearVelocity, double maxLinearAccel, double maxAngularVelocity, double maxAngularAccel) {
+        this.maxLinearVelocity = maxLinearVelocity;
+        this.maxLinearAccel = maxLinearAccel;
+        this.maxAngularVelocity = maxAngularVelocity;
+        this.maxAngularAccel = maxAngularAccel;
+    }
+
+    public double getMaxLinearVelocity() {
+        return maxLinearVelocity;
+    }
+
+    public double getMaxLinearAccel() {
+        return maxLinearAccel;
+    }
+
+    public double getMaxAngularVelocity() {
+        return maxAngularVelocity;
+    }
+
+    public double getMaxAngularAccel() {
+        return maxAngularAccel;
+    }
+
+    @Override
+    public String toString() {
+        return "ProfilingData{" +
+                "maxLinearVelocity=" + maxLinearVelocity +
+                ", maxLinearAccel=" + maxLinearAccel +
+                ", maxAngularVelocity=" + maxAngularVelocity +
+                ", maxAngularAccel=" + maxAngularAccel +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ProfilingData that = (ProfilingData) o;
+        return Double.compare(that.maxLinearVelocity, maxLinearVelocity) == 0 &&
+                Double.compare(that.maxLinearAccel, maxLinearAccel) == 0 &&
+                Double.compare(that.maxAngularVelocity, maxAngularVelocity) == 0 &&
+                Double.compare(that.maxAngularAccel, maxAngularAccel) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(maxLinearVelocity, maxLinearAccel, maxAngularVelocity, maxAngularAccel);
+    }
+}
