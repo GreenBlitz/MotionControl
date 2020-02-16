1768488b39578708bb783c5de874ee9b574a9fde
GBSoftwareLaptop <greenblitzfrc@gmail.com>
2020-02-16 08:14:24 +0200

    Code refactoring

--- motion/src/main/java/org/greenblitz/motion/profiling/ProfilingConfiguration.java
+++ motion/src/main/java/org/greenblitz/motion/profiling/ProfilingConfiguration.java
@@ -3,11 +3,10 @@ package org.greenblitz.motion.profiling;
 /**
  * @author Peleg
  */
-
 public class ProfilingConfiguration {
 
-    protected double velMultLin, accMultLin, jump, wheelPidKp, wheelPidKi, wheelPidKd, collapseConstaPerWheel, angPidKp, angPidKi, angPidKd, collapseConstAngular;
-    protected int smoothingTale;
+    protected double velMultLin, accMultLin, jump, wheelPidKp, wheelPidKi, wheelPidKd, collapseConstPerWheel, angPidKp, angPidKi, angPidKd, collapseConstAngular;
+    protected int smoothingTail;
 
     @Override
     public boolean equals(Object o) {
@@ -25,28 +24,28 @@ public class ProfilingConfiguration {
         if (Double.compare(that.angPidKp, angPidKp) != 0) return false;
         if (Double.compare(that.angPidKd, angPidKd) != 0) return false;
         if (Double.compare(that.angPidKi, angPidKi) != 0) return false;
-        if (Double.compare(that.collapseConstaPerWheel, collapseConstaPerWheel) != 0) return false;
+        if (Double.compare(that.collapseConstPerWheel, collapseConstPerWheel) != 0) return false;
         if (Double.compare(that.collapseConstAngular, collapseConstAngular) != 0) return false;
-        return smoothingTale == that.smoothingTale;
+        return smoothingTail == that.smoothingTail;
     }
 
     public ProfilingConfiguration(double velMultLin, double accMultLin, double jump,
                                   double wheelPidKp, double wheelPidKi, double wheelPidKd,
-                                  double collapseConstaPerWheel,
+                                  double collapseConstPerWheel,
                                   double angPidKp, double angPidKi, double angPidKd,
-                                  double collapseConstAngular, int smoothingTale) {
+                                  double collapseConstAngular, int smoothingTail) {
         this.velMultLin = velMultLin;
         this.accMultLin = accMultLin;
         this.jump = jump;
         this.wheelPidKp = wheelPidKp;
         this.wheelPidKi = wheelPidKi;
         this.wheelPidKd = wheelPidKd;
-        collapseConstaPerWheel = collapseConstaPerWheel;
+        this.collapseConstPerWheel = collapseConstPerWheel;
         this.angPidKp = angPidKp;
         this.angPidKi = angPidKi;
         this.angPidKd = angPidKd;
-        collapseConstAngular = collapseConstAngular;
-        this.smoothingTale = smoothingTale;
+        this.collapseConstAngular = collapseConstAngular;
+        this.smoothingTail = smoothingTail;
     }
 
     public double getVelMultLin() {
@@ -85,75 +84,75 @@ public class ProfilingConfiguration {
         return angPidKi;
     }
 
-    public double getCollapseConstaPerWheel() {
-        return collapseConstaPerWheel;
+    public double getCollapseConstPerWheel() {
+        return collapseConstPerWheel;
     }
 
     public double getCollapseConstAngular() {
         return collapseConstAngular;
     }
 
-    public int getSmoothingTale() {
-        return smoothingTale;
+    public int getSmoothingTail() {
+        return smoothingTail;
     }
 
     public ProfilingConfiguration setVelMultLin(double velMultLin) {
-        return new ProfilingConfiguration(velMultLin, accMultLin, jump, wheelPidKp, wheelPidKi, wheelPidKd, collapseConstaPerWheel,
-                angPidKp, angPidKi, angPidKd, collapseConstAngular, smoothingTale);
+        return new ProfilingConfiguration(velMultLin, accMultLin, jump, wheelPidKp, wheelPidKi, wheelPidKd, collapseConstPerWheel,
+                angPidKp, angPidKi, angPidKd, collapseConstAngular, smoothingTail);
     }
 
     public ProfilingConfiguration setAccMultLin(double accMultLin) {
-        return new ProfilingConfiguration(velMultLin, accMultLin, jump, wheelPidKp, wheelPidKi, wheelPidKd, collapseConstaPerWheel,
-                angPidKp, angPidKi, angPidKd, collapseConstAngular, smoothingTale);
+        return new ProfilingConfiguration(velMultLin, accMultLin, jump, wheelPidKp, wheelPidKi, wheelPidKd, collapseConstPerWheel,
+                angPidKp, angPidKi, angPidKd, collapseConstAngular, smoothingTail);
     }
 
     public ProfilingConfiguration setJump(double jump) {
-        return new ProfilingConfiguration(velMultLin, accMultLin, jump, wheelPidKp, wheelPidKi, wheelPidKd, collapseConstaPerWheel,
-                angPidKp, angPidKi, angPidKd, collapseConstAngular, smoothingTale);
+        return new ProfilingConfiguration(velMultLin, accMultLin, jump, wheelPidKp, wheelPidKi, wheelPidKd, collapseConstPerWheel,
+                angPidKp, angPidKi, angPidKd, collapseConstAngular, smoothingTail);
     }
 
     public ProfilingConfiguration setWheelPidKp(double wheelPidKp) {
-        return new ProfilingConfiguration(velMultLin, accMultLin, jump, wheelPidKp, wheelPidKi, wheelPidKd, collapseConstaPerWheel,
-                angPidKp, angPidKi, angPidKd, collapseConstAngular, smoothingTale);
+        return new ProfilingConfiguration(velMultLin, accMultLin, jump, wheelPidKp, wheelPidKi, wheelPidKd, collapseConstPerWheel,
+                angPidKp, angPidKi, angPidKd, collapseConstAngular, smoothingTail);
     }
 
     public ProfilingConfiguration setWheelPidKi(double wheelPidKi) {
-        return new ProfilingConfiguration(velMultLin, accMultLin, jump, wheelPidKp, wheelPidKi, wheelPidKd, collapseConstaPerWheel,
-                angPidKp, angPidKi, angPidKd, collapseConstAngular, smoothingTale);
+        return new ProfilingConfiguration(velMultLin, accMultLin, jump, wheelPidKp, wheelPidKi, wheelPidKd, collapseConstPerWheel,
+                angPidKp, angPidKi, angPidKd, collapseConstAngular, smoothingTail);
     }
 
     public ProfilingConfiguration setWheelPidKd(double wheelPidKd) {
-        return new ProfilingConfiguration(velMultLin, accMultLin, jump, wheelPidKp, wheelPidKi, wheelPidKd, collapseConstaPerWheel,
-                angPidKp, angPidKi, angPidKd, collapseConstAngular, smoothingTale);
+        return new ProfilingConfiguration(velMultLin, accMultLin, jump, wheelPidKp, wheelPidKi, wheelPidKd, collapseConstPerWheel,
+                angPidKp, angPidKi, angPidKd, collapseConstAngular, smoothingTail);
     }
 
-    public ProfilingConfiguration setCollapseConstaPerWheel(double collapseConstaPerWheel) {
-        return new ProfilingConfiguration(velMultLin, accMultLin, jump, wheelPidKp, wheelPidKi, wheelPidKd, collapseConstaPerWheel,
-                angPidKp, angPidKi, angPidKd, collapseConstAngular, smoothingTale);
+    public ProfilingConfiguration setCollapseConstPerWheel(double collapseConstPerWheel) {
+        return new ProfilingConfiguration(velMultLin, accMultLin, jump, wheelPidKp, wheelPidKi, wheelPidKd, collapseConstPerWheel,
+                angPidKp, angPidKi, angPidKd, collapseConstAngular, smoothingTail);
     }
 
     public ProfilingConfiguration setAngPidKp(double angPidKp) {
-        return new ProfilingConfiguration(velMultLin, accMultLin, jump, wheelPidKp, wheelPidKi, wheelPidKd, collapseConstaPerWheel,
-                angPidKp, angPidKi, angPidKd, collapseConstAngular, smoothingTale);
+        return new ProfilingConfiguration(velMultLin, accMultLin, jump, wheelPidKp, wheelPidKi, wheelPidKd, collapseConstPerWheel,
+                angPidKp, angPidKi, angPidKd, collapseConstAngular, smoothingTail);
     }
 
     public ProfilingConfiguration setAngPidKi(double angPidKi) {
-        return new ProfilingConfiguration(velMultLin, accMultLin, jump, wheelPidKp, wheelPidKi, wheelPidKd, collapseConstaPerWheel,
-                angPidKp, angPidKi, angPidKd, collapseConstAngular, smoothingTale);
+        return new ProfilingConfiguration(velMultLin, accMultLin, jump, wheelPidKp, wheelPidKi, wheelPidKd, collapseConstPerWheel,
+                angPidKp, angPidKi, angPidKd, collapseConstAngular, smoothingTail);
     }
 
     public ProfilingConfiguration setAngPidKd(double angPidKd) {
-        return new ProfilingConfiguration(velMultLin, accMultLin, jump, wheelPidKp, wheelPidKi, wheelPidKd, collapseConstaPerWheel,
-                angPidKp, angPidKi, angPidKd, collapseConstAngular, smoothingTale);
+        return new ProfilingConfiguration(velMultLin, accMultLin, jump, wheelPidKp, wheelPidKi, wheelPidKd, collapseConstPerWheel,
+                angPidKp, angPidKi, angPidKd, collapseConstAngular, smoothingTail);
     }
 
     public ProfilingConfiguration setCollapseConstAngular(double collapseConstAngular) {
-        return new ProfilingConfiguration(velMultLin, accMultLin, jump, wheelPidKp, wheelPidKi, wheelPidKd, collapseConstaPerWheel,
-                angPidKp, angPidKi, angPidKd, collapseConstAngular, smoothingTale);
+        return new ProfilingConfiguration(velMultLin, accMultLin, jump, wheelPidKp, wheelPidKi, wheelPidKd, collapseConstPerWheel,
+                angPidKp, angPidKi, angPidKd, collapseConstAngular, smoothingTail);
     }
 
-    public ProfilingConfiguration setSmoothingTale(int smoothingTale) {
-        return new ProfilingConfiguration(velMultLin, accMultLin, jump, wheelPidKp, wheelPidKi, wheelPidKd, collapseConstaPerWheel,
-                angPidKp, angPidKi, angPidKd, collapseConstAngular, smoothingTale);
+    public ProfilingConfiguration setSmoothingTail(int smoothingTail) {
+        return new ProfilingConfiguration(velMultLin, accMultLin, jump, wheelPidKp, wheelPidKi, wheelPidKd, collapseConstPerWheel,
+                angPidKp, angPidKi, angPidKd, collapseConstAngular, smoothingTail);
     }
 }
