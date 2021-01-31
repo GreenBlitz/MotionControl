558e18e242841db986b5b73d5169fc7a29e282d1
asaf8888 <asaf2048@gmail.com>
2021-01-31 20:55:43 +0200

    asaf- added profile generation function in live profiling follower

--- motion/src/main/java/org/greenblitz/motion/profiling/ReturnProfiler2D.java
+++ motion/src/main/java/org/greenblitz/motion/profiling/ReturnProfiler2D.java
@@ -8,15 +8,15 @@ import java.util.List;
 
 public class ReturnProfiler2D {
     public static  MotionProfile2D generateProfile(MotionProfile2D mainProfile, State startLocation, int indexOfMergeSegment,
-                                                   LinkedList.Node<MotionProfile2D.Segment2D> mergeSegmentNode, double jump, double velocityStart,
-                                                   double velocityEnd, double maxLinearVel, double maxAngularVel,
-                                                   double maxLinearAcc, double maxAngularAcc, double tStart,
-                                                   double tForCurve, int smoothingTail){
+                                                   LinkedList.Node<MotionProfile2D.Segment2D> mergeSegmentNode, double jump,
+                                                   double maxLinearVel, double maxAngularVel, double maxLinearAcc,
+                                                   double maxAngularAcc, double tStart, double tForCurve, int smoothingTail){
         ArrayList<State> locations = new ArrayList<State>(2);
         locations.add(startLocation);
         locations.add(mergeSegmentNode.getItem().getStateLocation());
-        MotionProfile2D returnProfile = ChassisProfiler2D.generateProfile(locations, jump, velocityStart, velocityEnd,
-                maxLinearVel, maxAngularVel, maxLinearAcc, maxAngularAcc, tStart, tForCurve, smoothingTail);
+        MotionProfile2D returnProfile = ChassisProfiler2D.generateProfile(locations, jump, locations.get(0).getLinearVelocity(),
+                locations.get(1).getLinearVelocity(), maxLinearVel, maxAngularVel, maxLinearAcc, maxAngularAcc, tStart,
+                tForCurve, smoothingTail);
 
         returnProfile.merge(mainProfile, indexOfMergeSegment, mergeSegmentNode);
         return returnProfile;
--- motion/src/main/java/org/greenblitz/motion/profiling/followers/LiveProfilingFollower2D.java
+++ motion/src/main/java/org/greenblitz/motion/profiling/followers/LiveProfilingFollower2D.java
@@ -4,6 +4,8 @@ import org.greenblitz.motion.Localizer;
 import org.greenblitz.motion.base.Position;
 import org.greenblitz.motion.base.State;
 import org.greenblitz.motion.profiling.MotionProfile2D;
+import org.greenblitz.motion.profiling.ReturnProfiler2D;
+import org.greenblitz.utils.LinkedList;
 
 public class LiveProfilingFollower2D {
 
@@ -27,7 +29,7 @@ public class LiveProfilingFollower2D {
 
     private static double calcError(MotionProfile2D profile, double currT, double linearVelocity,
                                     double angularVelocity, double kX, double kY, double kAngle){
-        State state = getLocation(linearVelocity, angularVelocity);
+        State state = new State(getLocation(), linearVelocity, angularVelocity);
         double currX = state.getX();
         double currY = state.getY();
         double currAngle = state.getAngle();
@@ -40,13 +42,22 @@ public class LiveProfilingFollower2D {
         return kX * (targetX  - currX) + kY * (targetY - currY) + kAngle * (targetAngle - currAngle);
     }
 
-    private static MotionProfile2D generateNewProfile(){
-
+    private static MotionProfile2D generateNewProfile(MotionProfile2D mainProfile, double maxLinearVel, double maxAngularVel,
+                                                      double maxLinearAcc, double maxAngularAcc, double tForCurve, double linearVelocity, double angularVelocity){
+        long  t = System.currentTimeMillis();
+        int indexOfMergeSegment = mainProfile.quickGetIndex(t+2000);
+        LinkedList.Node<MotionProfile2D.Segment2D> mergeSegmentNode = mainProfile.quickGetNode(t+2000);
+        return ReturnProfiler2D.generateProfile(
+                mainProfile, new State(getLocation(), linearVelocity, angularVelocity) , indexOfMergeSegment,
+                mergeSegmentNode, 0.01, System.currentTimeMillis(), maxLinearVel, maxAngularVel,
+                maxLinearAcc, maxAngularAcc, tForCurve, 4);
     }
 
-    private static State getLocation(double linearVelocity, double angularVelocity){
+    private static Position getLocation(){
         Localizer localizer = Localizer.getInstance();
-        Position pos = localizer.getLocation(); //TODO check if getLocation() or getLocationRaw()
-        return new State(pos, linearVelocity, angularVelocity);
+        return localizer.getLocation(); //TODO check if getLocation() or getLocationRaw()
+
     }
+
+
 }
