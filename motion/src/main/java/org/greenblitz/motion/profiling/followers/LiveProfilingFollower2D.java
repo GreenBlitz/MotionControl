1d15e44d95c8a47adce0f36b41d5ce58c47cbbce
Raz Rishoni <razrishoni@razs-mbp.home>
2021-01-31 20:50:12 +0200

    Raz - added get location and calc error in LiveProfilingFollower2D

--- motion/src/main/java/org/greenblitz/motion/profiling/followers/LiveProfilingFollower2D.java
+++ motion/src/main/java/org/greenblitz/motion/profiling/followers/LiveProfilingFollower2D.java
@@ -1,5 +1,8 @@
 package org.greenblitz.motion.profiling.followers;
 
+import org.greenblitz.motion.Localizer;
+import org.greenblitz.motion.base.Position;
+import org.greenblitz.motion.base.State;
 import org.greenblitz.motion.profiling.MotionProfile2D;
 
 public class LiveProfilingFollower2D {
@@ -22,12 +25,28 @@ public class LiveProfilingFollower2D {
 
     }
 
-    private static double calcError(){
+    private static double calcError(MotionProfile2D profile, double currT, double linearVelocity,
+                                    double angularVelocity, double kX, double kY, double kAngle){
+        State state = getLocation(linearVelocity, angularVelocity);
+        double currX = state.getX();
+        double currY = state.getY();
+        double currAngle = state.getAngle();
 
+        MotionProfile2D.Segment2D currSegment = profile.quickGetSegment(currT);
+        double targetX = currSegment.getStateLocation().getX();
+        double targetY = currSegment.getStateLocation().getY();
+        double targetAngle = currSegment.getStateLocation().getAngle();
+
+        return kX * (targetX  - currX) + kY * (targetY - currY) + kAngle * (targetAngle - currAngle);
     }
 
     private static MotionProfile2D generateNewProfile(){
 
     }
 
+    private static State getLocation(double linearVelocity, double angularVelocity){
+        Localizer localizer = Localizer.getInstance();
+        Position pos = localizer.getLocation(); //TODO check if getLocation() or getLocationRaw()
+        return new State(pos, linearVelocity, angularVelocity);
+    }
 }
