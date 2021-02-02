5bd9535a290ef6295b3fc83dc749a64e92efd609
Raz Rishoni <razrishoni@razs-mbp.home>
2021-02-02 20:27:14 +0200

    Raz - added updateProfile

--- motion/src/main/java/org/greenblitz/motion/profiling/followers/LiveProfilingFollower2D.java
+++ motion/src/main/java/org/greenblitz/motion/profiling/followers/LiveProfilingFollower2D.java
@@ -8,27 +8,38 @@ import org.greenblitz.motion.profiling.ReturnProfiler2D;
 import org.greenblitz.utils.LinkedList;
 
 public class LiveProfilingFollower2D {
+    private double destinationTimeOffset;
+    private double epsilon;
+    private double kX;
+    private double kY;
+    private double kAngle;
+    private MotionProfile2D profile;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    public static void updateProfile(){
-
+    public LiveProfilingFollower2D(MotionProfile2D profile, double epsilon, double kX, double kY,
+                                   double kAngle, double destinationTimeOffset){
+        this.profile = profile;
+        this.epsilon = epsilon;
+        this.kX = kX;
+        this.kY = kY;
+        this.kAngle = kAngle;
+        this.destinationTimeOffset = destinationTimeOffset;
     }
 
-    private static double calcError(MotionProfile2D profile, double currT, double linearVelocity,
-                                    double angularVelocity, double kX, double kY, double kAngle){
+    public LiveProfilingFollower2D(MotionProfile2D profile, double epsilon, double kX, double kY,
+                                   double kAngle){
+        this(profile,epsilon,kX,kY,kAngle,2000);
+    }
+
+    public void updateProfile(double currT, double maxLinearVel, double maxAngularVel,
+                              double maxLinearAcc, double maxAngularAcc, double tForCurve,
+                              double linearVelocity, double angularVelocity){
+        if(this.calcError(currT, linearVelocity, angularVelocity) > epsilon){
+            profile = generateNewProfile(maxLinearVel,maxAngularVel, maxLinearAcc,
+                    maxAngularAcc, tForCurve,linearVelocity,angularVelocity);
+        }
+    }
+
+    private double calcError(double currT, double linearVelocity, double angularVelocity){
         State state = new State(getLocation(), linearVelocity, angularVelocity);
         double currX = state.getX();
         double currY = state.getY();
@@ -39,16 +50,17 @@ public class LiveProfilingFollower2D {
         double targetY = currSegment.getStateLocation().getY();
         double targetAngle = currSegment.getStateLocation().getAngle();
 
-        return kX * (targetX  - currX) + kY * (targetY - currY) + kAngle * (targetAngle - currAngle);
+        return kX * (targetX - currX) + kY * (targetY - currY) + kAngle * (targetAngle - currAngle);
     }
 
-    private static MotionProfile2D generateNewProfile(MotionProfile2D mainProfile, double maxLinearVel, double maxAngularVel,
-                                                      double maxLinearAcc, double maxAngularAcc, double tForCurve, double linearVelocity, double angularVelocity){
-        long  t = System.currentTimeMillis();
-        int indexOfMergeSegment = mainProfile.quickGetIndex(t+2000);
-        LinkedList.Node<MotionProfile2D.Segment2D> mergeSegmentNode = mainProfile.quickGetNode(t+2000);
+    private MotionProfile2D generateNewProfile(double maxLinearVel, double maxAngularVel, double maxLinearAcc,
+                                               double maxAngularAcc, double tForCurve, double linearVelocity,
+                                               double angularVelocity){
+        long t = System.currentTimeMillis();
+        int indexOfMergeSegment = profile.quickGetIndex(t+destinationTimeOffset);
+        LinkedList.Node<MotionProfile2D.Segment2D> mergeSegmentNode = profile.quickGetNode(t+destinationTimeOffset);
         return ReturnProfiler2D.generateProfile(
-                mainProfile, new State(getLocation(), linearVelocity, angularVelocity) , indexOfMergeSegment,
+                profile, new State(getLocation(), linearVelocity, angularVelocity) , indexOfMergeSegment,
                 mergeSegmentNode, 0.01, System.currentTimeMillis(), maxLinearVel, maxAngularVel,
                 maxLinearAcc, maxAngularAcc, tForCurve, 4);
     }
@@ -56,8 +68,5 @@ public class LiveProfilingFollower2D {
     private static Position getLocation(){
         Localizer localizer = Localizer.getInstance();
         return localizer.getLocation(); //TODO check if getLocation() or getLocationRaw()
-
     }
-
-
 }
