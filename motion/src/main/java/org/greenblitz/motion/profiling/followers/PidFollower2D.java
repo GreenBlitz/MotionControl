8c54beaa6476c90af7ac6589852cf57ae0290460
asaf8888 <56229169+asaf8888@users.noreply.github.com>
2021-03-24 14:09:25 +0200

    Update PidFollower2D.java
    fixed error being the negative

--- motion/src/main/java/org/greenblitz/motion/profiling/followers/PidFollower2D.java
+++ motion/src/main/java/org/greenblitz/motion/profiling/followers/PidFollower2D.java
@@ -125,7 +125,7 @@ public class PidFollower2D extends AbstractFollower2D {
 
         if (sendData) {
             wheelTarget.report(timeNow, leftMotorV, leftCurr, rightMotorV, rightCurr);
-            errorTarget.report(timeNow,leftMotorV - leftCurr, rightMotorV - rightCurr);
+            errorTarget.report(timeNow,leftCurr - leftMotorV, rightCurr - rightMotorV);
             globalTarget.report(timeNow, velocity.getX(), (leftCurr + rightCurr) / 2.0, velocity.getY(),
                     (leftCurr - rightCurr) / wheelDist);
         }
