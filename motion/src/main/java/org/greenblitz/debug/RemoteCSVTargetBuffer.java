0efee0aaabeb2e9e8799d469bc6c2335e0aac822
Peleg <peleg.caduri@gmail.com>
2021-04-20 18:25:37 +0300

    Asaf - created csvBuffer which stores the data during runtime and graduatally releases it after the run. Added a mechanism which checks for dups (data was not updated) and linearly interpretes to get smoother results

---
+++ motion/src/main/java/org/greenblitz/debug/RemoteCSVTargetBuffer.java
@@ -0,0 +1,59 @@
+package org.greenblitz.debug;
+
+import java.util.ArrayList;
+
+public class RemoteCSVTargetBuffer {
+    RemoteCSVTarget target;
+    ArrayList<double[]> buffer;
+    private long delay;
+
+    public RemoteCSVTargetBuffer(String fileName, String... names){
+        this(fileName, 50, names);
+    }
+
+    public RemoteCSVTargetBuffer(String fileName, long delay, String... names){
+        target = RemoteCSVTarget.initTarget(fileName, names);
+        buffer = new ArrayList<double[]>();
+        this.delay = delay;
+    }
+
+    public void report(double... record){
+
+        buffer.add(record);
+    }
+
+    public void passToCSV(boolean resolveDupes){
+        if(resolveDupes) resolveDupes();
+        target.report(buffer.get(0).clone());
+        for(int i=0; i<buffer.size(); i++){
+            target.report(buffer.get(i));
+            try {
+                Thread.sleep(delay);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void resolveDupes(){
+        int time = 0;
+        for (int i = 0; i < buffer.get(0).length; i++) {
+            if(target.m_ntNames[i] == "time") time = i;
+        }
+
+        for (int i = 0; i < buffer.size() - 2; i++) {
+            for(int j = 0; j < buffer.get(i).length; j++){
+                if (buffer.get(i)[j] == buffer.get(i+1)[j]) {
+                    int l = i+2;
+                    while(l < buffer.size() - 1 && buffer.get(l)[j] ==buffer.get(i)[j]) l++;
+                    double dV = buffer.get(l)[j] - buffer.get(i)[j];
+                    double dT = buffer.get(l)[time] - buffer.get(i)[time];
+                    for (int k = i+1; k < l; k++) {
+                        double dt = buffer.get(k)[time] - buffer.get(i)[time];
+                        buffer.get(k)[j] = dV/dT*dt + buffer.get(i)[j];
+                    }
+                }
+            }
+        }
+    }
+}
